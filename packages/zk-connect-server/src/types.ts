8d532923e173f1ec48e3598404ddb2cef0c6794b
leo Sayous <leo@sismo.io>
2023-03-07 17:46:35 +0100

    feat: publishing 1.0.9 version with zk connect

--- packages/zk-connect-server/src/types.ts
+++ packages/zk-connect-server/src/types.ts
@@ -1,44 +1,82 @@
-export type TargetGroup = {
-    groupId: string;
-    timestamp?: number | 'latest';
-    value?: number | 'MAX';
-    additionalProperties?: any;
-}
-
-export type TargetComposedGroup = {
-    groups: TargetGroup;
-    operator: 'AND' | 'OR';
-}
-
-export type ZkConnectResponseRequest = {
+export type ZkConnectRequest = {
+    claim: DataRequest;
     appId: string;
-    serviceName: string;
-    targetGroup: TargetGroup | TargetComposedGroup;
+    serviceName?: string;
     callbackPath?: string;
     version: string;
+  };
+
+
+export enum ProvingScheme {
+    HYDRA_S1 = "hydra-s1",
 }
 
-export type ZkConnectResponse = { 
-    appId: string;
-    serviceName: string;
-    membershipProofs: Membership[]; 
-    version: string;
+export class DataRequest {
+  public statementRequests: StatementRequest[];
+  public operator: LogicalOperator;
+
+  constructor(args: {
+    statementRequests?: StatementRequest[];
+    operator?: LogicalOperator;
+    groupId?: string;
+    groupTimestamp?: string;
+    requestedValue?: number | "MAX";
+    comparator?: StatementComparator;
+    provingScheme?: ProvingScheme;
+  }) {
+    if(args.statementRequests) {
+        if(args.groupId) {
+            throw new Error("Cannot provide both statements and groupId");
+        }
+        if(args.groupTimestamp) {
+            throw new Error("Cannot provide both statements and groupTimestamp");
+        }
+        if(args.requestedValue) {
+            throw new Error("Cannot provide both statements and selectValue");
+        }
+        if(args.comparator) {
+            throw new Error("Cannot provide both statements and comparator");
+        }
+        if(args.provingScheme) {
+            throw new Error("Cannot provide both statements and provingScheme");
+        }
+    } else {
+        if(!args.groupId) {
+            throw new Error("Must provide groupId");
+        }
+    }
+
+    this.statementRequests = args.statementRequests || [{
+        groupId: args.groupId,
+        groupTimestamp: args.groupTimestamp ?? 'latest',
+        requestedValue: args.requestedValue ?? 1,
+        comparator: args.comparator ?? "HTE", 
+        provingScheme: args.provingScheme ?? ProvingScheme.HYDRA_S1, 
+    }];
+    this.operator = args.operator ?? null;
+  }
+}
+
+export type StatementRequest = {
+    groupId: string;
+    groupTimestamp?: string; // default to "latest"
+    requestedValue?: number | "MAX"; // If "MAX" the max value inside the group should be selected. The user can select what he wants to reveal 
+    comparator?: StatementComparator; // default to "HTE". , "EQ" . If requestedValue="MAX" comparator should be empty
+    provingScheme?: any; 
+  };
+export type StatementComparator = "HTE" | "EQ";
+
+
+export type VerifiableStatement = StatementRequest & { selectedValue: number, proof: string };
+export type VerifiedStatement = VerifiableStatement & { proofId: string };
+
+export type LogicalOperator = "AND" | "OR";
+
+export type ZkConnectResponse = Omit<ZkConnectRequest, "callbackPath"> & {
+  verifiableStatements: VerifiableStatement[];
 };
 
-export type Membership = {
-    proofId: string;
-    groupId: string;
-    value: number;
-    timestamp: number | 'latest';
-    additionalProperties?: Record<string, any>;
-    provingScheme: string;
-    version: string;
-    proof: any; 
-}
-
-export type PwsReceipt = {
-    proofId?: string;
-    proofIds: string[];
-    provedMembership?: Membership;
-    provedMemberships: Membership[];
+export type ZkconnectVerifiedResult = ZkConnectResponse & {
+  vaultId: string;
+  verifiedStatements: VerifiedStatement[];
 };
