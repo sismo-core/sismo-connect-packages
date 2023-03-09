71275c7c1005db168a796d1e36ce356c02a4907d
leo Sayous <leo@sismo.io>
2023-03-09 10:56:03 +0100

    feat: add devMode option and eligible group data override

--- packages/zk-connect-client/src/types.ts
+++ packages/zk-connect-client/src/types.ts
@@ -1,14 +1,13 @@
 export type ZkConnectRequest = {
-    claim: DataRequest;
-    appId: string;
-    namespace?: string;
-    callbackPath?: string;
-    version: string;
-  };
-
+  claim: DataRequest;
+  appId: string;
+  namespace?: string;
+  callbackPath?: string;
+  version: string;
+};
 
 export enum ProvingScheme {
-    HYDRA_S1 = "hydra-s1.2",
+  HYDRA_S1 = "hydra-s1.2",
 }
 
 export class DataRequest {
@@ -23,52 +22,63 @@ export class DataRequest {
     requestedValue?: number | "USER_SELECTED_VALUE";
     comparator?: StatementComparator;
     provingScheme?: ProvingScheme;
+    extraData?: StatementRequestExtraData;
   }) {
-    if(args.statementRequests) {
-        if(args.groupId) {
-            throw new Error("Cannot provide both statements and groupId");
-        }
-        if(args.groupTimestamp) {
-            throw new Error("Cannot provide both statements and groupTimestamp");
-        }
-        if(args.requestedValue) {
-            throw new Error("Cannot provide both statements and selectValue");
-        }
-        if(args.comparator) {
-            throw new Error("Cannot provide both statements and comparator");
-        }
-        if(args.provingScheme) {
-            throw new Error("Cannot provide both statements and provingScheme");
-        }
+    if (args.statementRequests) {
+      if (args.groupId) {
+        throw new Error("Cannot provide both statements and groupId");
+      }
+      if (args.groupTimestamp) {
+        throw new Error("Cannot provide both statements and groupTimestamp");
+      }
+      if (args.requestedValue) {
+        throw new Error("Cannot provide both statements and selectValue");
+      }
+      if (args.comparator) {
+        throw new Error("Cannot provide both statements and comparator");
+      }
+      if (args.provingScheme) {
+        throw new Error("Cannot provide both statements and provingScheme");
+      }
     } else {
-        if(!args.groupId) {
-            throw new Error("Must provide groupId");
-        }
+      if (!args.groupId) {
+        throw new Error("Must provide groupId");
+      }
     }
 
-    this.statementRequests = args.statementRequests || [{
+    this.statementRequests = args.statementRequests || [
+      {
         groupId: args.groupId,
-        groupTimestamp: args.groupTimestamp ?? 'latest',
+        groupTimestamp: args.groupTimestamp ?? "latest",
         requestedValue: args.requestedValue ?? 1,
         comparator: args.comparator ?? "GTE",
-        provingScheme: args.provingScheme ?? ProvingScheme.HYDRA_S1, 
-    }];
+        provingScheme: args.provingScheme ?? ProvingScheme.HYDRA_S1,
+        extraData: args.extraData,
+      },
+    ];
     this.operator = args.operator ?? null;
   }
 }
 
-
 export type StatementRequest = {
   groupId: string;
   groupTimestamp?: number | "latest"; // default to "latest"
-  requestedValue?: number | "USER_SELECTED_VALUE"; // If "MAX" the max value inside the group should be selected. The user can select what he wants to reveal 
+  requestedValue?: number | "USER_SELECTED_VALUE"; // If "MAX" the max value inside the group should be selected. The user can select what he wants to reveal
   comparator?: StatementComparator; // default to "GTE". , "EQ" If requestedValue="USER_SELECTED_VALUE" comparator "GTE"
-  provingScheme?: any; 
+  provingScheme?: any;
+  extraData?: StatementRequestExtraData;
+};
+
+export type StatementRequestExtraData = {
+  devModeOverrideEligibleGroupData?: { [accountIdentifier: string]: number };
 };
 
 export type StatementComparator = "GTE" | "EQ";
 
-export type VerifiableStatement = StatementRequest & { value: number, proof: any };
+export type VerifiableStatement = StatementRequest & {
+  value: number;
+  proof: any;
+};
 export type VerifiedStatement = VerifiableStatement & { proofId: string };
 
 export type LogicalOperator = "AND" | "OR";
--- packages/zk-connect-client/src/zk-connect.ts
+++ packages/zk-connect-client/src/zk-connect.ts
@@ -2,50 +2,95 @@ import { DEFAULT_BASE_URL, VERSION } from "./constants";
 import { DataRequest, ZkConnectResponse } from "./types";
 
 export type ZkConnectParams = {
-    appId: string;
-    opts?: {
-        vaultAppBaseUrl?: string;
-    }
-}
+  appId: string;
+  opts?: {
+    isDevMode?: boolean;
+    vaultAppBaseUrl?: string;
+  };
+};
 
 export type RequestParams = {
-    dataRequest?: DataRequest;
-    namespace?: string;
-    callbackPath?: string;
-}
+  dataRequest?: DataRequest;
+  namespace?: string;
+  callbackPath?: string;
+};
 
 export class ZkConnect {
-    private _appId: string;
-    private _vaultAppBaseUrl: string;
+  private _appId: string;
+  private _vaultAppBaseUrl: string;
+  private _isDevMode: boolean;
 
-    constructor({ appId, opts }: ZkConnectParams) {
-        this._appId = appId;
-        this._vaultAppBaseUrl = opts?.vaultAppBaseUrl || DEFAULT_BASE_URL;
+  constructor({ appId, opts }: ZkConnectParams) {
+    this._appId = appId;
+    this._vaultAppBaseUrl = opts?.vaultAppBaseUrl || DEFAULT_BASE_URL;
+    this._isDevMode = opts?.isDevMode ?? false;
+    if (this._isDevMode) {
+      console.warn(
+        "zkConnect launch in DevMode! Never use this mode in production!"
+      );
     }
+  }
 
-    public request = ({ dataRequest, namespace, callbackPath }: RequestParams = {})  => {
-        if (!window) throw new Error(`requestProof is not available outside of a browser`);
+  public request = ({
+    dataRequest,
+    namespace,
+    callbackPath,
+  }: RequestParams = {}) => {
+    if (!window)
+      throw new Error(`requestProof is not available outside of a browser`);
 
-        let url = `${this._vaultAppBaseUrl}/connect?version=${VERSION}&appId=${this._appId}`;
+    let url = `${this._vaultAppBaseUrl}/connect?version=${VERSION}&appId=${this._appId}`;
 
-        if(dataRequest) {
-            url += `&dataRequest=${JSON.stringify(dataRequest)}`;
+    if (dataRequest) {
+      for (const statementRequest of dataRequest.statementRequests) {
+        const devModeOverrideEligibleGroupData =
+          statementRequest.extraData?.devModeOverrideEligibleGroupData;
+        console.log(
+          "devModeOverrideEligibleGroupData",
+          devModeOverrideEligibleGroupData
+        );
+        if (devModeOverrideEligibleGroupData) {
+          if (!this._isDevMode) {
+            console.error(`
+devModeOverrideEligibleGroupData can only be used in DevMode!
+
+Use this option to enable it: 
+
+const zkConnect = new ZkConnect({
+    appId: YOUR_APP_ID,
+    opts: {
+      isDevMode: true, // <<--------  Here
+    }
+});
+`);
+            return;
+          }
+          console.info(
+            `Eligible group data for groupId ${statementRequest.groupId} is overridden with:`,
+            devModeOverrideEligibleGroupData
+          );
         }
-
-        if (callbackPath) {
-            url += `&callbackPath=${callbackPath}`;
-        }
-        url += `&namespace=${ namespace ?? 'main'}`;
-
-        window.location.href = encodeURI(url);
+      }
+      url += `&dataRequest=${JSON.stringify(dataRequest)}`;
     }
 
-    public getResponse = (): ZkConnectResponse | null => {
-        if (!window) throw new Error(`getResponse is not available outside of a browser`);
-        const url = new URL(window.location.href);
-        if (url.searchParams.has("zkConnectResponse")) {
-            return JSON.parse(url.searchParams.get("zkConnectResponse") as string) as ZkConnectResponse;
-        }   
-        return null;
+    if (callbackPath) {
+      url += `&callbackPath=${callbackPath}`;
     }
+    url += `&namespace=${namespace ?? "main"}`;
+
+    window.location.href = encodeURI(url);
+  };
+
+  public getResponse = (): ZkConnectResponse | null => {
+    if (!window)
+      throw new Error(`getResponse is not available outside of a browser`);
+    const url = new URL(window.location.href);
+    if (url.searchParams.has("zkConnectResponse")) {
+      return JSON.parse(
+        url.searchParams.get("zkConnectResponse") as string
+      ) as ZkConnectResponse;
+    }
+    return null;
+  };
 }
