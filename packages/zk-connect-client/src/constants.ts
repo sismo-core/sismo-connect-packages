6c0a96a159fa8dcd208ad5adda3f625350276dca
Dev <gabinhry@gmail.com>
2023-03-14 16:33:06 +0100

    fix: update client package version

--- packages/zk-connect-client/src/constants.ts
+++ packages/zk-connect-client/src/constants.ts
@@ -1,3 +1,3 @@
 export const PROD_VAULT_APP_BASE_URL = `https://vault-beta.sismo.io`;
 export const DEV_VAULT_APP_BASE_URL = `https://dev.vault-beta.sismo.io`;
-export const VERSION = `off-chain-1`;
+export const VERSION = `zk-connect-v1`;
