126934750aa72c3e30a2c0e1293f60fe89d61920
bigq <99990370+yum0e@users.noreply.github.com>
2023-04-06 14:00:52 +0200

    fix: add a modulo snark field after the hash to prevent snark field (#19)
    * fix: add a modulo snark field after the hash to prevent snark field
    overflow when verifying
    
    * feat: bump version to 0.0.5

--- packages/zk-connect-server/src/verifier/hydras2-verifier.ts
+++ packages/zk-connect-server/src/verifier/hydras2-verifier.ts
@@ -1,5 +1,5 @@
 import { CommitmentMapperRegistryContractDev } from './libs/contracts/commitment-mapper-registry/dev';
-import { HydraS2Verifier as HydraS2VerifierPS } from "@sismo-core/hydra-s2";
+import { HydraS2Verifier as HydraS2VerifierPS, SNARK_FIELD } from "@sismo-core/hydra-s2";
 import {
   GNOSIS_AVAILABLE_ROOTS_REGISTRY_ADDRESS,
   GNOSIS_COMMITMENT_MAPPER_REGISTRY_ADDRESS
@@ -204,13 +204,13 @@ export class HydraS2Verifier {
     } else {
       extraData = ethers.utils.toUtf8Bytes(proof.signedMessage);
     }
-
-    const signedMessage = ethers.utils.keccak256(extraData);
+    const signedMessage = BigNumber.from(ethers.utils.keccak256(extraData)).mod(SNARK_FIELD);
+    
     if (!BigNumber.from(proofPublicInputs.extraData).eq(signedMessage)) {
       throw new Error(
         `on proofId "${proofIdentifier}" extraData "${
           BigNumber.from(proofPublicInputs.extraData).toHexString()
-        }" mismatch with signedMessage "${signedMessage}"`
+        }" mismatch with signedMessage "${signedMessage.toHexString()}"`
       );
     }
   }
