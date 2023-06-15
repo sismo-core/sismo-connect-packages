501c69d0fc31bc2cca261ce7cbbf8c6dce6faa21
Dev <gabinhry@gmail.com>
2023-06-15 16:39:24 +0200

    fix: update impersonate mode url

--- packages/sismo-connect-client/tests/sismo-connect.test.ts
+++ packages/sismo-connect-client/tests/sismo-connect.test.ts
@@ -68,7 +68,7 @@ describe('Sismo Connect Client', () => {
         claims: [claim],
       })
     ).toEqual(
-      `https://dev.vault-beta.sismo.io/connect?version=sismo-connect-v1.1&appId=0xf68985adfc209fafebfb1a956913e7fa&claims=[{\"groupId\":\"0x1\",\"claimType\":0,\"extraData\":\"\",\"groupTimestamp\":\"latest\",\"value\":1}]&vault={\"impersonate\":[\"0x123\",\"0x345\"]}&compressed=true`
+      `https://vault-beta.sismo.io/connect?version=sismo-connect-v1.1&appId=0xf68985adfc209fafebfb1a956913e7fa&claims=[{\"groupId\":\"0x1\",\"claimType\":0,\"extraData\":\"\",\"groupTimestamp\":\"latest\",\"value\":1}]&vault={\"impersonate\":[\"0x123\",\"0x345\"]}&compressed=true`
     )
   })
 })
