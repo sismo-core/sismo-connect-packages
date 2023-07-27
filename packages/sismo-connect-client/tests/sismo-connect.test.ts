8bc62919a86c0ffa5d1deab596b7d15a58b1ee31
gabin54 <59560717+gabin54@users.noreply.github.com>
2023-07-27 12:01:28 +0200

    Feat: add new sismo connect button (#47)
    Co-authored-by: Benjamin A <97291322+Baoufa@users.noreply.github.com>

--- packages/sismo-connect-client/tests/sismo-connect.test.ts
+++ packages/sismo-connect-client/tests/sismo-connect.test.ts
@@ -1,5 +1,12 @@
 import { beforeAll, describe, expect, it } from "@jest/globals";
-import { ClaimRequest, SismoConnect, SismoConnectClient, SismoConnectConfig } from "../src";
+import {
+  Auth,
+  AuthType,
+  ClaimRequest,
+  SismoConnect,
+  SismoConnectClient,
+  SismoConnectConfig,
+} from "../src";
 
 describe("Sismo Connect Client", () => {
   let appId: string;
@@ -51,5 +58,25 @@ describe("Sismo Connect Client", () => {
     expect(sismoConnect.getRequestLink({ claims: [claim] })).toEqual(
       `https://vault-beta.sismo.io/connect?version=sismo-connect-v1.1&appId=0xf68985adfc209fafebfb1a956913e7fa&claims=[{\"groupId\":\"0x1\",\"claimType\":0,\"extraData\":\"\",\"groupTimestamp\":\"latest\",\"value\":1}]&vault={\"impersonate\":[\"0x123\",\"0x345\"]}&compressed=true`
     );
+  });
+
+  it("should put EVM account userIds in lower case", async () => {
+    const requestLink = sismoConnect.getRequestLink({
+      auths: [
+        {
+          authType: AuthType.EVM_ACCOUNT,
+          userId: "0xAa1BcC",
+        },
+        {
+          authType: AuthType.EVM_ACCOUNT,
+          userId: "0xDaDBcD",
+        },
+      ],
+    });
+    const url = new URL(requestLink);
+    let params = new URLSearchParams(url.search);
+    let auths = JSON.parse(params.get("auths") as string) as Auth[];
+    expect(auths[0].userId).toEqual("0xaa1bcc");
+    expect(auths[1].userId).toEqual("0xdadbcd");
   });
 });
